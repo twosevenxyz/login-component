0702f1db576c6ad0a8c15f7703af6e6e04b2962b
Guru Prasad Srinivasa <gurupras@buffalo.edu>
2019-10-22 08:04:00 -0400

    Added storybook

---
+++ src/stories/index.stories.js
@@ -0,0 +1,82 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import { storiesOf } from '@storybook/vue'
+import { action } from '@storybook/addon-actions'
+import { withKnobs, text, boolean, object } from '@storybook/addon-knobs'
+
+import '../style/bulma-imports.scss'
+
+import LoginComponent from '../components/LoginComponent.vue'
+import InputElement from '../components/InputElement.vue'
+
+storiesOf('InputElement', module)
+  .add('default', () => ({
+    components: { InputElement },
+    template: '<InputElement @submit="onSubmit"/>',
+    methods: { action: action('onSubmit') }
+  }))
+  .add('type=number', () => ({
+    components: { InputElement },
+    template: '<InputElement type="number" @submit="onSubmit"/>',
+    methods: { action: action('onSubmit') }
+  }))
+  .add('placeholder', () => ({
+    components: { InputElement },
+    template: '<InputElement placeholder="test" @submit="onSubmit"/>',
+    methods: { action: action('onSubmit') }
+  }))
+
+storiesOf('LoginComponent', module)
+  .addDecorator(withKnobs)
+  .add('default', () => ({
+    components: { LoginComponent },
+    template: `
+  <div id="app">
+    <LoginComponent :show.sync="showModal" :initialized="initialized" :theme="theme" :logged-in-id="loggedInId" :social="social" :logo="logo" :app-name="appName" @submit="onSubmit" :error="error" :info="info" :is-submitting.sync="isSubmitting"/>
+  </div>`,
+    methods: {
+      onSubmit: action('onSubmit')
+    },
+    props: {
+      showModal: {
+        default: boolean('ShowModal', false)
+      },
+      initialized: {
+        default: boolean('initialized', false)
+      },
+      theme: {
+        default: {
+          background: text('background', '#009688'),
+          text: text('text', '#fff'),
+          invertedText: text('invertedText', '#000')
+        }
+      },
+      loggedInId: {
+        default: object('loggedInId', undefined)
+      },
+      error: {
+        default: text('Error', '')
+      },
+      info: {
+        default: text('Info', '')
+      },
+      logo: {
+        default: text('Logo', `/security_shield.png`)
+      },
+      appName: {
+        default: text('App name', 'Storybook')
+      },
+      isSubmitting: {
+        default: boolean('IsSubmitting', false)
+      },
+      social: {
+        providers: {
+          google: boolean('Google', false),
+          facebook: boolean('Facebook', false)
+        }
+      }
+    },
+    data () {
+      return {
+      }
+    }
+  }))
